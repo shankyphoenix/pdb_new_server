a57d3efeee3fe5ce7e44b7741a87d81cae6468e6
Shanky <shanky@interlynxsystems.com>
2026-03-13 18:19:39 +0530

    auth fixed.

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -15,8 +15,6 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         parent::boot();
 
-        Horizon::middleware(['auth']);
-
         // Horizon::routeSmsNotificationsTo('15556667777');
         // Horizon::routeMailNotificationsTo('example@example.com');
         // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
